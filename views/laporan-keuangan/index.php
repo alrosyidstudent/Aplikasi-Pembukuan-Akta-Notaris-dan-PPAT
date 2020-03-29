283ff4feb90a8f5f849d5817fa2f6ea60e50c662
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-03-29 16:43:17 +0700

    membuat button navigasi untuk laporan keuangan

--- views/laporan-keuangan/index.php
+++ views/laporan-keuangan/index.php
@@ -14,3 +14,27 @@ $this->params['breadcrubs'][] = $this->title;
  	<h1><?= Html::encode($this->title) ?></h1>
  	<?php  ?>
  </div>
+  <div class="panel panel-primary">
+		<div class="panel-heading"></div>
+	
+	  		<div class="x_panel">
+	  			<div class="row">
+	  				<div class="col-sm-6">
+	  				Laporan Neraca
+	  				</div>
+	  				<div class="col-sm-6" align="right">
+	  					<?= Html::a('Lihat Laporan', ['neraca'], ['class' => 'btn btn-success']) ?>
+	  				</div>
+	  			</div>
+
+	  			<div class="row">
+	  				<div class="col-sm-6">
+	  				Laporan Laba Rugi
+	  				</div>
+	  				<div class="col-sm-6" align="right">
+	  					<?= Html::a('Lihat Laporan', ['labarugi'], ['class' => 'btn btn-success']) ?>
+	  				</div>
+	  			</div>
+	  			
+			</div>
+	</div>
