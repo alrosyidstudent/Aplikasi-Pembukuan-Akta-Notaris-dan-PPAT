90fffb5bf77102073e13c728b2dfae6f5edee5d4
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-09 21:12:14 +0700

    membuat tabel untuk menampilkan data dari tabel transaksi ke halaman transaksi pemasukan

--- views/transaksi/index.php
+++ views/transaksi/index.php
@@ -19,178 +19,45 @@ $this->params['breadcrubs'][] = $this->title;
  	<?php  ?>
  </div>
 	<p>
-      <?= Html::a('Transaksi Pengeluaran', ['pengeluaran'], ['class' => 'btn btn-success']) ?>
+      <?= Html::a('Pengeluaran', ['pengeluaran'], ['class' => 'btn btn-success']) ?>
+
+       <?= Html::a('Tambah Transaksi', ['create'], ['class' => 'btn btn-success']) ?>
+    
     </p>
+    
+     
 
-    <div class="panel panel-primary">
-		<div class="panel-heading"></div>
-	
-	  		<div class="x_panel">
+<div class="panel panel-primary">
+    <div class="panel-heading">List Pemasukan</div>   
+      <div class="x_panel">
+    
+        <table class="table table-bordered table-hover">
+            <tr class="success">
+                <td>Tanggal</td>
+                <td>Jenis Transaksi</td>
+                <td>Nominal</td>
+                <td>Keterangan</td>
+                <td>Aksi</td>
 
-	  			<?= $this->render('_form', [
-            	'model' => $model,
-        		]) ?>
-			</div>
-	</div>
-
-
-
-
-
-
-	<div class="panel-heading"> List Pemasukan</div>	
-	<div class="x_panel">
-
-		<?php 
-
-		if (Yii::$app->user->identity->role == User::ROLE_NOTARIS) {
-        $gridColumns = [
-            /*[
-                'class' => 'kartik\grid\SerialColumn',
-                'contentOptions' => ['style' => 'width: 10px;'],
-            ],*/
-            [
-                'attribute' => 'nomor',
-                'vAlign' => 'left',
-                'contentOptions' => ['style' => 'width: 20px;'],
-            ],
-            [
-                'attribute' => 'Keterangan Biaya',
-                'vAlign' => 'left',
-                'contentOptions' => ['style' => 'width: 250px;'],
-            ],
-            [
-                'class' => '\kartik\grid\DataColumn',
-                'attribute' => 'Kategori Akun',
-                'contentOptions' => ['style' => 'width: 150px;'],
                 
-            ],
 
-            [
-                'attribute' => 'tanggal',
-                'contentOptions' => ['style' => 'width: 50px;'],
-                'format' => ['date', 'php:Y-m-d'],
-                'filterType' => '\kartik\widgets\DatePicker',
-                'filterWidgetOptions' => [
-                    'pluginOptions' => [
-                        'format' => 'yyyy-mm-dd',
-                        'weekStart' => '1',
-                        'language' => 'uk',
-                    ],
-                ],
-            ],
-            [
-                'class' => '\kartik\grid\DataColumn',
-                'attribute' => 'Nominal',
-                'contentOptions' => ['style' => 'width: 100px;'],
-                'pageSummary' => true,
-                
-            ],
-            
-            ['class' => 'yii\grid\ActionColumn',
-                'contentOptions' => ['style' => 'width: 100px;'],
-                'buttons' => [
-                    'proses' => function ($url, $model, $key) {
-                        return Html::a('<span class="fa fa-list-ul" aria-hidden="true"></span> ', ['akta-notaris-proses/create', 'akta_notaris_id' => $model->id]);
-                    },
-                    /*'pihak' => function ($url, $model, $key) {
-                        return Html::a('<span class="fa fa-user-o" aria-hidden="true"></span> ', ['akta-notaris/pihak', 'id' => $model->id]);
-                    },*/
-                ],
-                'template' => '{proses} {pihak} {update} {view}'
-            ],
-        ];
-    } else {
-        $gridColumns = [
-            /*[
-                'class' => 'kartik\grid\SerialColumn',
-                'contentOptions' => ['style' => 'width: 10px;'],
-            ],*/
-            [
-                'attribute' => 'nomor',
-                'vAlign' => 'left',
-                'contentOptions' => ['style' => 'width: 20px;'],
-            ],
-            [
-                'attribute' => 'nama',
-                'vAlign' => 'left',
-                'contentOptions' => ['style' => 'width: 250px;'],
-            ],
-            [
-                'class' => '\kartik\grid\DataColumn',
-                'attribute' => 'jenis',
-                'contentOptions' => ['style' => 'width: 150px;'],
-                'pageSummary' => true,
-                // 'filterType' => GridView::FILTER_SELECT2,
-                // 'filterWidgetOptions' => [
-                //     'pluginOptions' => [
-                //         'data' => $data_jenis,
-                //         'autoWidget' => true,
-                //         'autoclose' => true,
-                //     ]
-                // ],
-            ],
+            </tr>
+            <?php 
+            foreach ($dataTransaksi as $transaksi) {
+                # code...
+                ?>
+                <tr>
+                    <td><?=$transaksi->tanggal ?></td>
+                     <td><?=$transaksi->labelJenis() ?></td>
+                    <td><?=$transaksi->nominal ?></td>
+                    <td><?=$transaksi->keterangan ?></td>
+                </tr>
+            <?php 
+            }
+            ?>
 
-            [
-                'attribute' => 'tanggal',
-                'contentOptions' => ['style' => 'width: 50px;'],
-                'format' => ['date', 'php:Y-m-d'],
-                'filterType' => '\kartik\widgets\DatePicker',
-                'filterWidgetOptions' => [
-                    'pluginOptions' => [
-                        'format' => 'yyyy-mm-dd',
-                        'weekStart' => '1',
-                        'language' => 'uk',
-                    ],
-                ],
-            ],
+        </table>
 
-            [
-                'attribute' => 'pic',
-                'vAlign' => 'left',
-                'format' => 'raw',
-                'contentOptions' => ['style' => 'width: 100px;'],
-              ],
-            ['class' => 'yii\grid\ActionColumn',
-                'contentOptions' => ['style' => 'width: 100px;'],
-                'buttons' => [
-                ],
-                'template' => '{view}'
-            ],
-        ];
-    }
-
-
-    // Pjax::begin();
-    // echo GridView::widget([
-    //     'id' => 'kv-grd-demo',
-    //     'dataProvider' => $dataProvider,
-    //     'filterModel' => $searchModel,
-    //     'columns' => $gridColumns,
-    //     'containerOptions' => ['style' => 'overflow: auto'], // only set when $responsive = false
-    //     'pjax' => true, // pjax is set to always true for this demo
-    //     'toolbar' => [
-    //     ],
-    //     'export' => [
-    //         'fontAwesome' => true
-    //     ],
-    //     'bordered' => false,
-    //     'striped' => true,
-    //     'condensed' => true,
-    //     'responsive' => true,
-    //     'hover' => true,
-
-    //     'showPageSummary' => false,
-    //     'panel' => [
-    //         'type' => GridView::TYPE_PRIMARY,
-    //     ],
-    //     'persistResize' => false,
-    //     'exportConfig' => false,
-    // ]);
-
-
-		 ?>
-
+    </div>
 </div>
-
 
