9dbde4dedc791a8d42a8ba1809c033b3128019d3
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-13 09:25:59 +0700

    memperbaiki rules dan get data dari tabel yang berelas dengan tabel akun di models/Akun

--- models/Akun.php
+++ models/Akun.php
@@ -3,21 +3,10 @@
 namespace app\models;
 
 use Yii;
-use yii\base\Models;
-use app\models\Transaksi;
-use app\models\KategoriAkun;
+
 use yii\db\ActiveRecord;
 
-class Akun extends \yii\db\ActiveRecord
-{
-
-	
-	public $nama_akun;
-	public $debit;
-	public $kredit;
-    public $kode;
-    public $kategori;
-    public $keteranngan;
+class Akun extends \yii\db\ActiveRecord{
 
 	public static function tableName()
 	
@@ -27,101 +16,55 @@ class Akun extends \yii\db\ActiveRecord
 	}
 
 
-	// public function rules()
-	// {
+	public function rules()
+    {
 
-	// return [
- //            [['id','notaris_id', 'akta_ppat_id','akta_notaris_id','akta_badan_id','akun_id'], 'required'],
- //            [['notaris_id', 'nominal','akun_id'], 'integer'],
- //            [['tanggal'], 'safe'],
- //            [['kategori'], 'string', 'max' => 45],           
- //            // [['notaris_id'], 'exist', 'skipOnError' => true, 'targetClass' => Notaris::className(), 'targetAttribute' => ['notaris_id' => 'id']],
- //        ];
-	// }
+	return [
+            [['id','notaris_id','kategori_akun_id'], 'required'],
+            [['notaris_id', 'kredit','debit','kategori_akun_id'], 'integer'],             
+            [['nama'], 'string', 'max' => 45],  
+            [['ket'], 'string', 'max' => 200],         
+            [['notaris_id'], 'exist', 'skipOnError' => true, 'targetClass' => Notaris::className(), 'targetAttribute' => ['notaris_id' => 'id']],
+            [['kategori_akun_id'], 'exist', 'skipOnError' => true, 'targetClass' => KategoriAkun::className(), 'targetAttribute' => ['kategori_akun_id' => 'id']]
+        ];
+	}
 
 
 	public function attributesLabels()
 	{
 		 return [
             'id' => 'ID',
-            'nama_akun' => 'Nama Akun',
+            'nama' => 'Nama Akun',
             'debit' => 'Debit',
             'kredit' => 'kredit',
             'notaris_id' => 'Nama Notaris',
-            'kode'=>'kode',
-            'kategori' => 'Kategori Akun',
-            'keterangan'=>'Deskripsi',           
+            'kategori_akun_id' => 'Kategori Akun',
+            'ket'=>'Keterangan Akun'       
         ];
 
 	}
 
-
-    
-
-    public function dataKategoriAkun(){
-        return[
-            1=>'Kas & Bank',
-            2=>'Aktiva Lancar'
-        ];
-
+/**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getNotaris()
+    {
+        return $this->hasOne(Notaris::className(), ['id' => 'notaris_id']);
     }
 
- //      public static function getOptions(){
- //        $data= 'biaya terkait akta';
-       
- //        return $data;
- //    }
 
-	// /**
- //     * @return \yii\db\ActiveQuery
- //     */
- //    public function getNotaris()
- //    {
- //        return $this->hasOne(Notaris::className(), ['id' => 'notaris_id']);
- //    }
-    
-
-	// /**
- //     * @return \yii\db\ActiveQuery
- //     */
- //    public function getAktaPPAT()
- //    {
- //        return $this->hasOne(AktaPpat::className(), ['id' => 'akta_ppat_id']);
- //    }
-   
-
- //    /**
- //     * @return \yii\db\ActiveQuery
- //     */
- //    public function getAktaNotaris()
- //    {
- //        return $this->hasOne(AktaNotaris::className(), ['id' => 'akta_notaris_id']);
- //    }
-    
-
- //    /**
- //     * @return \yii\db\ActiveQuery
- //     */
- //    public function getAktaBadan()
- //    {
- //        return $this->hasOne(AktaBadan::className(), ['id' => 'akta_badan_id']);
- //    }
-
-    
-
- //    /**
- //     * @return \yii\db\ActiveQuery
- //     */
- //    public function getAkun()
- //    {
- //        return $this->hasOne(Akun::className(), ['id' => 'akun_id']);
- //    }
- //    public  function getNama()
- //    {
- //        return $this->akun->nama_kategori;
- //    }
-
-   
+   /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getKategoriAkun()
+    {
+        return $this->hasOne(KategoriAkun::className(), ['id' => 'kategori_akun_id']);
+    }
+    public  function getKategori()
+    {
+        return $this->KategoriAkun->name;
+    }
+	
 
 }
 
