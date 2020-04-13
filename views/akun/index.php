aec4e096f1e1077d455fd749a715a2ccf7dccf6c
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-13 09:24:20 +0700

    membuat halaman utama untuk kelola akun

--- views/akun/index.php
+++ views/akun/index.php
@@ -4,26 +4,70 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
 use app\models\User;
-use app\models\akun;
+use app\models\Akun;
+use yii\widgets\ActiveForm;
+use kartik\date\DatePicker;
+use kartik\widgets\Select2;
 
 
 
-$this->title='Kelola Akun';
+$this->title='kelola akun';
 $this->params['breadcrubs'][] = $this->title;
  ?>
  <div class="akun">
  	<h1><?= Html::encode($this->title) ?></h1>
- 	<?php ?>
+ 	<?php  ?>
  </div>
+	<p>
+     
 
- 	<p>
-        <?= Html::a('Tambah Akun Baru', ['create'], ['class' => 'btn btn-success']) ?>
-        <?= Html::a('Atur Akun', ['update'], ['class' => 'btn btn-success']) ?>
+       <?= Html::a('Tambah akun', ['create'], ['class' => 'btn btn-success']) ?>
+    
     </p>
-    <div class="panel panel-primary">
-		<div class="panel-heading"></div>
-	
-	  		<div class="x_panel">
-	  			AKun
-			</div>
-	</div>
+    
+     
+
+<div class="panel panel-primary">
+    <div class="panel-heading">List Pemasukan</div>   
+      <div class="x_panel">
+    
+        <table class="table table-bordered table-hover">
+            <tr class="success" align="center">
+                <td>nama</td>
+                <td>debit</td>
+                <td>kredit</td>
+                <td>Keterangan</td>
+                <td>Aksi</td>
+
+                
+
+            </tr>
+            <?php 
+            foreach ($dataAkun as $akun) {
+                # code...
+                ?>
+                <tr>
+                    <td><?=$akun->nama ?></td>
+                     <td><?=$akun->debit ?></td>
+                    <td>Rp.<?=$akun->kredit ?>,-</td>
+                    <td><?=$akun->ket ?></td>
+                    <td align="center"> 
+                        <?= Html::a('Hapus', ['delete', 'id' => $akun->id], [
+                         'class' => 'btn btn-danger',
+                            'data' => [
+                                'confirm' => 'Apakah anda yakin akan menghapus akun ini?',
+                                'method' => 'post',
+                                    ],
+                                ]) ?>
+                             <?= Html::a('Perbaharui', ['update', 'id' => $akun->id], ['class' => 'btn btn-primary']) ?>
+                    </td>
+                </tr>
+            <?php 
+            }
+            ?>
+
+        </table>
+
+    </div>
+</div>
+
