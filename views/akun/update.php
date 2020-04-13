510636dbd72cefce19ff1913905a870d38170399
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-13 09:23:53 +0700

    membuat halaman update akun

--- views/akun/update.php
+++ views/akun/update.php
@@ -1,51 +1,25 @@
-<?php 
+<?php
+
 use yii\helpers\Html;
-use yii\grid\GridView;
-use yii\widgets\Pjax;
-use app\models\User;
-use app\models\akun;
-use yii\widgets\ActiveForm;
-use kartik\widgets\Select2;
-use kartik\date\DatePicker;
-
-$this->title='Atur Akun';
-$this->params['breadcrubs'][] = $this->title;
- ?>
- <div class="akun">
- 	<h1><?= Html::encode($this->title) ?></h1>
- 	<?php  ?>
- </div>
- 	<p>
-			<?= Html::a('Kembali', ['index'], ['class' => 'btn btn-danger']) ?>
-			Silahkan masukkan saldo awal Pertanggal
-			<?php
-            // echo '<label>Tanggal</label>';
-            // echo DatePicker::widget([
-            //     'model' => $model,
-            //     'attribute' => 'tanggal',
-            //     'name' => 'tanggal',
-            //     'value' => date('Y-m-d', strtotime('+30 days')),
-            //     'options' => ['placeholder' => 'Pilih tanggal'],
-            //     'pluginOptions' => [
-            //         'format' => 'yyyy-mm-dd',
-            //         'autoclose' => true,
-            //         'todayHighlight' => true
-            //     ]
-            // ]);
-            ?>
-	</p>
-
-    <div class="panel panel-primary">
-		<div class="panel-heading"></div>
-	
-	  		<div class="x_panel">
-	  			ini halaman atur akun
-
-	  			<div class="col-sm-12" align="right">
-	  				 <?= Html::submitButton('Simpan',['class' => 'btn btn-success'])?>
-	  			</div>
-			</div>
-	</div>
 
 
- ?>
+/* @var $this yii\web\View */
+/* @var $model app\models\AktaNotaris */
+
+
+$this->params['breadcrumbs'][] = ['label' => 'Transaski', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="akun-update">
+
+    <h2>Tambah Transaksi</h2>
+    
+
+    <div class="x_panel">
+
+        <?= $this->render('_form', [
+            'model' => $model,
+        ]) ?>
+    </div>
+
+</div>
