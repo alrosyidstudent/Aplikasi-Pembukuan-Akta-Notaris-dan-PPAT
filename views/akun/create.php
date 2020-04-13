d2a07318db9b58daf07b171a8d430e53b975c013
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-13 09:24:53 +0700

    membuat halaman create data akun

--- views/akun/create.php
+++ views/akun/create.php
@@ -1,29 +1,25 @@
-<?php 
+<?php
+
 use yii\helpers\Html;
-use yii\grid\GridView;
-use yii\widgets\Pjax;
-use app\models\User;
-use app\models\akun;
-use yii\widgets\ActiveForm;
-use kartik\widgets\Select2;
 
-$this->title='Tambah Akun';
-$this->params['breadcrubs'][] = $this->title;
- ?>
- <div class="akun">
- 	<h1><?= Html::encode($this->title) ?></h1>
- 	<?php  ?>
- </div>
 
-    <div class="panel panel-primary">
-		<div class="panel-heading"></div>
-	
-	  		<div class="x_panel">
+/* @var $this yii\web\View */
+/* @var $model app\models\AktaNotaris */
 
-	  			<?= $this->render('_form', [
-            	'model' => $model,
-        		]) ?>
-			</div>
-	</div>
 
- ?>
+$this->params['breadcrumbs'][] = ['label' => 'akun', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="akun-create">
+
+    <h2>Tambah akun</h2>
+  
+
+    <div class="x_panel">
+
+        <?= $this->render('_form', [
+            'model' => $model,
+        ]) ?>
+    </div>
+
+</div>
