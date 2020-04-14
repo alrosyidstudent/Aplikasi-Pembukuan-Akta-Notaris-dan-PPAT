ac5bddc262d694303a4e14427a23bdb4d20fb67d
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-14 20:06:42 +0700

    Menampilkan data pengeluaran menggunakan generate Gii

--- views/transaksi/pengeluaran.php
+++ views/transaksi/pengeluaran.php
@@ -1,76 +1,45 @@
-<?php 
+<?php
 
 use yii\helpers\Html;
 use yii\grid\GridView;
-use yii\widgets\Pjax;
-use app\models\User;
-use app\models\transaksi;
-use yii\widgets\ActiveForm;
-use kartik\date\DatePicker;
-use kartik\widgets\Select2;
 
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
+$this->title = 'Transaksi Keluar';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="pengeluaran">
 
-$this->title='Transaksi Pengeluaran';
-$this->params['breadcrubs'][] = $this->title;
- ?>
- <div class="transaksi">
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php  ?>
- </div>
-    <p>
-      <?= Html::a('Pemasukan', ['index'], ['class' => 'btn btn-success']) ?>
 
-      <?= Html::a('Tambah Transaksi', ['create2'], ['class' => 'btn btn-success']) ?>
+    <p>
+        <?= Html::a('Tambah Transaksi', ['create'], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Transaksi Masuk', ['index'], ['class' => 'btn btn-success']) ?>
     </p>
 
-<div class="panel panel-primary">
-    <div class="panel-heading">List Pengeluaran</div>
-    
-      <div class="x_panel">
-         <table class="table table-hover">
-            <tr class="success" align="center">
-                <td>No</td>
-                <td>Tanggal</td>
-                <td>Jenis Transaksi</td>
-                <td>Nominal</td>
-                <td>Keterangan</td>
-                <td>Aksi</td>
 
-                
+    <div class="panel panel-primary">
+        <div class="panel-heading">List Pengeluaran</div>   
+            <div class="x_panel">
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
 
-            </tr>
-            <?php 
-            $no=1;
-            foreach ($dataTransaksi as $transaksi) {
-                # code...
-                ?>
-                <tr>
-                    <td><?php echo $no ?></td>
-                    <td><?=$transaksi->tanggal ?></td>
-                     <td><?=$transaksi->labelJenis() ?></td>
-                    <td>Rp.<?=$transaksi->nominal ?>,-</td>
-                    <td><?=$transaksi->keterangan ?></td>
-                    <td align="center"> 
-                        <?= Html::a('Hapus', ['delete2', 'id' => $transaksi->id], [
-                         'class' => 'btn btn-danger',
-                            'data' => [
-                                'confirm' => 'Apakah anda yakin akan menghapus transaksi ini?',
-                                'method' => 'post',
-                                    ],
-                                ]) ?>
-                             <?= Html::a('Perbaharui', ['update2', 'id' => $transaksi->id], ['class' => 'btn btn-primary']) ?>
-                    </td>
-                </tr>
-            <?php 
-            $no++;
-            }
-            ?>
+            'nominal',
+            'tanggal',
+            'keterangan',
+            // 'notaris_id',
+            // 'akta_ppat_id',
+            // 'akta_notaris_id',
+            // 'akta_badan_id',
+            // 'kategori_akun_id',
+            // 'register',
 
-        </table>
-    </div>
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
 </div>
-
-
-
-
+</div>
+</div>
