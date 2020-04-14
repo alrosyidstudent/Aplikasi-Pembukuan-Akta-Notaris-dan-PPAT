13da9581f36fbe9c3c1df114faa184983b649b42
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-14 16:40:11 +0700

    memperbaiki form untuk melakukan input transaksi masuk dan keluar

--- views/transaksi/_form.php
+++ views/transaksi/_form.php
@@ -2,7 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
 use yii\helpers\ArrayHelper;
 use kartik\widgets\DepDrop;
 use yii\helpers\Url;
@@ -10,75 +9,58 @@ use kartik\date\DatePicker;
 use kartik\widgets\Select2;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\AktaBadan */
+/* @var $model app\models\Transaksi */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="transaksi-form">
 
-  <?php $form = ActiveForm::begin();
+    <?php $form = ActiveForm::begin(); 
+    echo $form->field($model, "notaris_id")->hiddenInput(['value' => Yii::$app->user->identity->notaris_id])->label(false);
 
-        echo $form->field($model, "notaris_id")->hiddenInput(['value' => Yii::$app->user->identity->notaris_id])->label(false);
     ?>
-    <div class="row">
-        <div class="col-sm-2">
-           <?= $form->field($model, 'nominal')->textInput(['maxlength' => true]) ?>
-        
-        </div>
 
-         <div class="col-sm-2">
-           <?php
-            // echo $form->field($model, 'akun_id')->widget(Select2::classname(), [
-            //     'data' => \app\models\Transaksi::getOptions(),
-            //     'id' => 'akun_id',
-            //     'options' => ['placeholder' => 'Pilih..'],
-            //     'pluginOptions' => [
-            //         'allowClear' => true
-            //     ],
-            // ]);
-            ?>
-        
-        </div>
+    <?= $form->field($model,'jenis')->dropDownList($model->dataJenisTransaksi(),[
+                'class'=>'form-control','prompt'=>'pilih jenis..'
+            ])?>
 
-         <div class="col-sm-2">
-           
-        
-        </div>
+    <?= $form->field($model, 'nominal')->textInput() ?>
 
+    <?php
+        echo '<label>Tanggal</label>';
+        echo DatePicker::widget([
+            'model' => $model,
+            'attribute' => 'tanggal',
+            'name' => 'tanggal',
+            'value' => date('Y-m-d', strtotime('+30 days')),
+            'options' => ['placeholder' => 'Pilih tanggal'],
+            'pluginOptions' => [
+                'format' => 'yyyy-mm-dd',
+                'autoclose' => true,
+                'todayHighlight' => true
+            ]
+        ]);
+    ?>
 
-        <div class="col-sm-3">
-            <?php
-            echo '<label>Tanggal</label>';
-            echo DatePicker::widget([
-                'model' => $model,
-                'attribute' => 'tanggal',
-                'name' => 'tanggal',
-                'value' => date('Y-m-d', strtotime('+30 days')),
-                'options' => ['placeholder' => 'Pilih tanggal'],
-                'pluginOptions' => [
-                    'format' => 'yyyy-mm-dd',
-                    'autoclose' => true,
-                    'todayHighlight' => true
-                ]
-            ]);
-            ?>
-        </div>
+    <?= $form->field($model, 'keterangan')->textInput(['maxlength' => true]) ?>
 
-         <div class="col-sm-2">
-            <?= $form->field($model, 'keterangan')->textInput() ?>
-        
-        </div>
-            <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Simpan' : 'Perbaharui', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-             </div>
+ 
+    <?php
+        echo $form->field($model, 'kategori_akun_id')->widget(Select2::classname(), [
+            'data' => \app\models\KategoriAkun::getOptions(),
+            'id' => 'kategori_akun_id',
+            'options' => ['placeholder' => 'Pilih..'],
+            'pluginOptions' => [
+                'allowClear' => true
+            ],
+        ]);
+    ?>
+    
 
-
-
-
-   
-       
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Simpan' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
 
     <?php ActiveForm::end(); ?>
-
 
 </div>
