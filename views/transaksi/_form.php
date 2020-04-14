17e0aa4ecc35b27bf4c0b1098c0bd8cd7f9bd1be
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-14 19:27:57 +0700

    Mengubah form menggunakan generate Gii

--- views/transaksi/_form.php
+++ views/transaksi/_form.php
@@ -9,88 +9,58 @@ use kartik\date\DatePicker;
 use kartik\widgets\Select2;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\AktaBadan */
+/* @var $model app\models\Transaksi */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="transaksi-form">
 
-    <?php $form = ActiveForm::begin([
-      
-    ]);
+    <?php $form = ActiveForm::begin(); 
+    echo $form->field($model, "notaris_id")->hiddenInput(['value' => Yii::$app->user->identity->notaris_id])->label(false);
 
-        echo $form->field($model, "notaris_id")->hiddenInput(['value' => Yii::$app->user->identity->notaris_id])->label(false);
     ?>
 
-    <div class="row">
-        <div class="col-sm-6">
-           <?= $form->field($model, 'nominal')->textInput(['maxlength' => true]) ?>
-
-        </div>
-    </div>
-
-
-    <div class="row">        
-         <div class="col-sm-6">
-
-            <?= $form->field($model,'jenis')->dropDownList($model->dataJenisTransaksi(),[
+    <?= $form->field($model,'jenis')->dropDownList($model->dataJenisTransaksi(),[
                 'class'=>'form-control','prompt'=>'pilih jenis..'
             ])?>
-         </div>
+
+    <?= $form->field($model, 'nominal')->textInput() ?>
+
+    <?php
+        echo '<label>Tanggal</label>';
+        echo DatePicker::widget([
+            'model' => $model,
+            'attribute' => 'tanggal',
+            'name' => 'tanggal',
+            'value' => date('Y-m-d', strtotime('+30 days')),
+            'options' => ['placeholder' => 'Pilih tanggal'],
+            'pluginOptions' => [
+                'format' => 'yyyy-mm-dd',
+                'autoclose' => true,
+                'todayHighlight' => true
+            ]
+        ]);
+    ?>
+
+    <?= $form->field($model, 'keterangan')->textInput(['maxlength' => true]) ?>
+
+ 
+    <?php
+        echo $form->field($model, 'kategori_akun_id')->widget(Select2::classname(), [
+            'data' => \app\models\KategoriAkun::getOptions(),
+            'id' => 'kategori_akun_id',
+            'options' => ['placeholder' => 'Pilih..'],
+            'pluginOptions' => [
+                'allowClear' => true
+            ],
+        ]);
+    ?>
+    
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Simpan' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
-
-
-    <div class="row">
-         <div class="col-sm-6">
-            <?php
-            echo $form->field($model, 'kategori_akun_id')->widget(Select2::classname(), [
-                'data' => \app\models\KategoriAkun::getOptions(),
-                'id' => 'kategori_akun_id',
-                'options' => ['placeholder' => 'Pilih..'],
-                'pluginOptions' => [
-                    'allowClear' => true
-                ],
-            ]);
-            ?>
-        </div>
-    </div>
-
-
-    <div class="row">
-        <div class="col-sm-6">
-            <?php
-            echo '<label>Tanggal</label>';
-            echo DatePicker::widget([
-                'model' => $model,
-                'attribute' => 'tanggal',
-                'name' => 'tanggal',
-                'value' => date('Y-m-d', strtotime('+30 days')),
-                'options' => ['placeholder' => 'Pilih tanggal'],
-                'pluginOptions' => [
-                    'format' => 'yyyy-mm-dd',
-                    'autoclose' => true,
-                    'todayHighlight' => true
-                ]
-            ]);
-            ?>
-            
-        </div>
-    </div>
-
-    <div class="row">
-         <div class="col-sm-6">
-           <?= $form->field($model, 'keterangan')->textInput(['maxlength' => true]) ?>
-
-        </div>
-    </div>
-
-
-            <div class="col-sm-12">
-        <?= Html::submitButton($model->isNewRecord ? 'Simpan' : 'Perbaharui', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-            </div>
 
     <?php ActiveForm::end(); ?>
 
-
-</div>
 </div>
