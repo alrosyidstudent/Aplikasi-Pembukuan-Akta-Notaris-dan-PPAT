e573dcc72964cbac74ab7d16e7a772b903f63563
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-14 17:31:16 +0700

    Mengubah Fungsi untuk CRUD pada TransaksiController

--- controllers/TransaksiController.php
+++ controllers/TransaksiController.php
@@ -1,135 +1,93 @@
-<?php 
+<?php
 
 namespace app\controllers;
 
 use Yii;
 use app\models\Transaksi;
-use app\models\TransaksiSearch;
-use app\models\AktaBadan;
-use app\models\AktaPpat;
-use app\models\AktaNotaris;
-
-use app\models\KategoriAkun;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
-use yii\data\Pagination;
-use yii\filters\VerbFilter;
 use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
-
-
-
+/**
+ * TransaksiController implements the CRUD actions for Transaksi model.
+ */
 class TransaksiController extends Controller
-
 {
-
+    /**
+     * @inheritdoc
+     */
     public function behaviors()
-        {
-            return [
-                'verbs' => [
-                    'class' => VerbFilter::className(),
-                    'actions' => [
-                        'delete' => ['POST'],
-                    ],
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
                 ],
-            ];
-        }
-
-
-    public function actionModal()
-    {
-        return $this->render('modal');
+            ],
+        ];
     }
 
-
-
-    public function actionModalt()
-    {
-        $model=new Transaksi();
-        if ($model->load(Yii::$app->request->post())) {
-            if ($model->save()) {
-                if (Yii::$app->request->isAjax) {
-                    // JSON response is expected in case of successful save
-                    Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-                    return ['success' => true];
-                }
-                return $this->redirect(['index', 'id' => $model->id]);
-            }
-        }
-
-        if (Yii::$app->request->isAjax) {
-            return $this->renderAjax('create', [
-                'model' => $model,
-            ]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-
+    /**
+     * Lists all Transaksi models.
+     * @return mixed
+     */
     public function actionIndex()
-        {
-        // $searchModel = new TransaksiSearch();
-        // $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+    {
+        $search = Yii::$app->request->queryParams;
+        $query= Transaksi::find()
+            ->where(['jenis'=>1]);
+            //->joinWith('keterangan');
 
-       
-        // return $this->render('index', [
-        //     'searchModel' => $searchModel,
-        //     //'dataProvider' => $dataProvider,
-        // ]);
-            $model = new Transaksi;
-            $dataTransaksi = Transaksi::find()
-            ->where(['jenis'=>1])
-            ;
-         return $this->render('index',compact('dataTransaksi','model'));
-        }
-        
+       //  if (!empty($search['keterangan'])) {
+       // $query->andFilterWhere(['like','keterangan',$search['keterangan']]);
+        $dataProvider = new ActiveDataProvider([
+            'query' =>$query,
+        ]);
+        //}
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
 
     public function actionPengeluaran()
     {
-        $model = new Transaksi;
-        $dataTransaksi = Transaksi::find()
-        ->where(['jenis'=>2])
-        ->all();
-         return $this->render('pengeluaran',compact('dataTransaksi'));
+        $dataProvider = new ActiveDataProvider([
+            'query' => Transaksi::find()
+            ->where(['jenis'=>2])
+        ]);
+
+        return $this->render('pengeluaran', [
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
+    /**
+     * Displays a single Transaksi model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
 
-
-     public function actionFilter()
-        {
-        // $searchModel = new TransaksiSearch();
-        // $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-       
-        // return $this->render('index', [
-        //     'searchModel' => $searchModel,
-        //     //'dataProvider' => $dataProvider,
-        // ]);
-            $model = new Transaksi();
-            $dataTransaksi = Transaksi::find()
-            ->where(['tanggal'=>'2020-04-29'])
-            ->all();
-         return $this->render('index',compact('dataTransaksi','model'));
-        }
-
-    
-
-
-     public function actionCreate()
+    /**
+     * Creates a new Transaksi model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
     {
         $model = new Transaksi();
-
-        
-        if ($model->load(Yii::$app->request->post())) {
-            $model->register = $this->generateRegister("register");
-            $model->insert();
-            /*echo "<pre>";
-            print_r($model);
-            echo "<pre>";*/
-            return $this->redirect(['index', 'id' => $model->id]);
-            
+        //$model->register = $this->generateRegister("register");
+        if ($model->load(Yii::$app->request->post()) && $model->insert()) {
+            return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('create', [
                 'model' => $model,
@@ -137,43 +95,45 @@ class TransaksiController extends Controller
         }
     }
 
+    //  public function generateRegister($attribute, $length = 8) {
 
-    public function generateRegister($attribute, $length = 8) {
+    //     $randomString = strtoupper(Yii::$app->getSecurity()->generateRandomString($length));
 
-        $randomString = strtoupper(Yii::$app->getSecurity()->generateRandomString($length));
+    //     if((!$reg_model=AktaPpat::findOne([$attribute => $randomString]))
+    //         and (!$reg_model=AktaBadan::findOne([$attribute => $randomString]))
+    //         and (!$reg_model=AktaNotaris::findOne([$attribute => $randomString]))
+    //     )
+    //         return $randomString;
+    //     else
+    //         return $this->generateRegister($attribute, $length);
 
-        if((!$reg_model=AktaPpat::findOne([$attribute => $randomString]))
-            and (!$reg_model=AktaBadan::findOne([$attribute => $randomString]))
-            and (!$reg_model=AktaNotaris::findOne([$attribute => $randomString]))
-        )
-            return $randomString;
-        else
-            return $this->generateRegister($attribute, $length);
+    // }
 
-    }
-
-
-     public function actionCreate2()
+    /**
+     * Updates an existing Transaksi model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
     {
-        $model = new Transaksi();
+        $model = $this->findModel($id);
 
-        
-        if ($model->load(Yii::$app->request->post())) {
-            $model->register = $this->generateRegister("register");
-            $model->insert();
-            /*echo "<pre>";
-            print_r($model);
-            echo "<pre>";*/
-            return $this->redirect(['pengeluaran', 'id' => $model->id]);
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
         } else {
-            return $this->render('create', [
+            return $this->render('update', [
                 'model' => $model,
             ]);
         }
     }
 
-
-
+    /**
+     * Deletes an existing Transaksi model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
     public function actionDelete($id)
     {
         $this->findModel($id)->delete();
@@ -181,58 +141,19 @@ class TransaksiController extends Controller
         return $this->redirect(['index']);
     }
 
-
-    public function actionDelete2($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['pengeluaran']);
-    }
-
-
-
-     public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['index', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-
-    public function actionUpdate2($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['pengeluaran', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
+    /**
+     * Finds the Transaksi model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Transaksi the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
     protected function findModel($id)
-        {
-            if (($model = Transaksi::findOne($id)) !== null) {
-                return $model;
-            } else {
-                throw new NotFoundHttpException('The requested page does not exist.');
-            }
+    {
+        if (($model = Transaksi::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
         }
-            
-
-   
-   
-
- 
+    }
 }
-
-
- ?>
