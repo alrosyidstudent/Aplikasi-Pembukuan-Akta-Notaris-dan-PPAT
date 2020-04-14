3a4d950d518f3d1fcb96401eaa846ea903355e67
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-14 16:42:19 +0700

    memperbaiki file models dari generate Gii

--- models/Transaksi.php
+++ models/Transaksi.php
@@ -1,62 +1,80 @@
-<?php 
+<?php
 
 namespace app\models;
 
 use Yii;
-use app\models\Transaksi;
-use yii\db\ActiveRecord;
 
+/**
+ * This is the model class for table "transaksi".
+ *
+ * @property integer $id
+ * @property string $jenis
+ * @property integer $nominal
+ * @property string $tanggal
+ * @property string $keterangan
+ * @property integer $notaris_id
+ * @property integer $akta_ppat_id
+ * @property integer $akta_notaris_id
+ * @property integer $akta_badan_id
+ * @property integer $kategori_akun_id
+ * @property string $register
+ *
+ * @property Notaris $notaris
+ * @property AktaPpat $aktaPpat
+ * @property AktaNotaris $aktaNotaris
+ * @property AktaBadan $aktaBadan
+ * @property KategoriAkun $kategoriAkun
+ */
 class Transaksi extends \yii\db\ActiveRecord
 {
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'transaksi';
+    }
 
-	
-	// public $nominal;
- //    public $kategori;
- //    public $tanggal;
-    
-
-
-	public static function tableName()
-	
-	{
-		return 'transaksi';
-	}
-
-
-	public function rules()
-	{
-
-	return [
-            [['notaris_id', 'kategori_akun_id'], 'required'],
-            [['notaris_id', 'nominal','kategori_akun_id'], 'integer'],          
-            [['keterangan','jenis'], 'string', 'max' => 45],           
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['jenis', 'nominal', 'tanggal', 'keterangan', 'notaris_id', 'kategori_akun_id'], 'required'],
+            [['nominal', 'notaris_id', 'akta_ppat_id', 'akta_notaris_id', 'akta_badan_id', 'kategori_akun_id'], 'integer'],
+            [['tanggal'], 'safe'],
+            [['jenis'], 'string', 'max' => 45],
+            [['keterangan'], 'string', 'max' => 100],
+            
             [['notaris_id'], 'exist', 'skipOnError' => true, 'targetClass' => Notaris::className(), 'targetAttribute' => ['notaris_id' => 'id']],
             [['akta_ppat_id'], 'exist', 'skipOnError' => true, 'targetClass' => AktaPpat::className(), 'targetAttribute' => ['akta_ppat_id' => 'id']],
             [['akta_notaris_id'], 'exist', 'skipOnError' => true, 'targetClass' => AktaNotaris::className(), 'targetAttribute' => ['akta_notaris_id' => 'id']],
             [['akta_badan_id'], 'exist', 'skipOnError' => true, 'targetClass' => AktaBadan::className(), 'targetAttribute' => ['akta_badan_id' => 'id']],
             [['kategori_akun_id'], 'exist', 'skipOnError' => true, 'targetClass' => KategoriAkun::className(), 'targetAttribute' => ['kategori_akun_id' => 'id']],
-            [['tanggal'], 'safe'    ],
         ];
-	}
+    }
 
-
-	public function attributesLabels()
-	{
-		 return [
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
             'id' => 'ID',
-            'jenis' => 'Jenis Transaksi',
+            'jenis' => 'Jenis',
             'nominal' => 'Nominal',
             'tanggal' => 'Tanggal',
-            'notaris_id' => 'Nama Notaris',
-            'akta_ppat_id' => 'Akta PPAT',
-            'akta_notaris_id' => 'Akta Notaris',
-            'akta_badan_id' => 'Akta Badan',
-            'kategori_akun_id' => 'Kategori',
-            'register' => 'Register',
-            'keterangan'=>'Keterangan'
+            'keterangan' => 'Keterangan',
+            'notaris_id' => 'Notaris ID',
+            'akta_ppat_id' => 'Akta Ppat ID',
+            'akta_notaris_id' => 'Akta Notaris ID',
+            'akta_badan_id' => 'Akta Badan ID',
+            'kategori_akun_id' => 'Kategori Akun',
+            
         ];
+    }
 
-	}
 
     public function dataJenisTransaksi(){
         return[
@@ -78,23 +96,22 @@ class Transaksi extends \yii\db\ActiveRecord
 
     }
 
-	/**
+
+    /**
      * @return \yii\db\ActiveQuery
      */
     public function getNotaris()
     {
         return $this->hasOne(Notaris::className(), ['id' => 'notaris_id']);
     }
-    
 
-	/**
+    /**
      * @return \yii\db\ActiveQuery
      */
-    public function getAktaPPAT()
+    public function getAktaPpat()
     {
         return $this->hasOne(AktaPpat::className(), ['id' => 'akta_ppat_id']);
     }
-   
 
     /**
      * @return \yii\db\ActiveQuery
@@ -103,7 +120,6 @@ class Transaksi extends \yii\db\ActiveRecord
     {
         return $this->hasOne(AktaNotaris::className(), ['id' => 'akta_notaris_id']);
     }
-    
 
     /**
      * @return \yii\db\ActiveQuery
@@ -113,7 +129,6 @@ class Transaksi extends \yii\db\ActiveRecord
         return $this->hasOne(AktaBadan::className(), ['id' => 'akta_badan_id']);
     }
 
-    
     /**
      * @return \yii\db\ActiveQuery
      */
@@ -121,11 +136,8 @@ class Transaksi extends \yii\db\ActiveRecord
     {
         return $this->hasOne(KategoriAkun::className(), ['id' => 'kategori_akun_id']);
     }
-    public  function getKategori()
+     public  function getKategori()
     {
         return $this->KategoriAkun->name;
     }
-    
 }
-
- ?>
