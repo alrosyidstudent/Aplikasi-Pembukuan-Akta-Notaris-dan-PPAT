1d1d529b68a3f5adc1b8b2cb909982d92f04ea0b
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-14 20:52:58 +0700

    a

--- views/akun/_form.php
+++ views/akun/_form.php
@@ -9,39 +9,70 @@ use kartik\date\DatePicker;
 use kartik\widgets\Select2;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\Akun */
+/* @var $model app\models\AktaBadan */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="akun-form">
 
-    <?php $form = ActiveForm::begin(); 
-    echo $form->field($model, "notaris_id")->hiddenInput(['value' => Yii::$app->user->identity->notaris_id])->label(false);
+    <?php $form = ActiveForm::begin([
+      
+    ]);
+
+        echo $form->field($model, "notaris_id")->hiddenInput(['value' => Yii::$app->user->identity->notaris_id])->label(false);
     ?>
 
-    <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'debit')->textInput() ?>
+    <div class="row">
+        <div class="col-sm-6">
 
-    <?= $form->field($model, 'kredit')->textInput() ?>
+           <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
 
-    <?php
-        echo $form->field($model, 'kategori_akun_id')->widget(Select2::classname(), [
-            'data' => \app\models\KategoriAkun::getOptions(),
-            'id' => 'kategori_akun_id',
-            'options' => ['placeholder' => 'Pilih..'],
-            'pluginOptions' => [
-                'allowClear' => true
-            ],
-        ]);
-    ?>
-
-    <?= $form->field($model, 'ket')->textInput(['maxlength' => true]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
     </div>
 
+
+    <div class="row">        
+         <div class="col-sm-6">
+
+            <?= $form->field($model, 'debit')->textInput(['maxlength' => true]) ?>
+         </div>
+    </div>
+
+<div class="row">
+        <div class="col-sm-6">
+        <?= $form->field($model, 'kredit')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+         <div class="col-sm-6">
+            <?php
+            echo $form->field($model, 'kategori_akun_id')->widget(Select2::classname(), [
+                'data' => \app\models\KategoriAkun::getOptions(),
+                'id' => 'kategori_akun_id',
+                'options' => ['placeholder' => 'Pilih..'],
+                'pluginOptions' => [
+                    'allowClear' => true
+                ],
+            ]);
+            ?>
+        </div>
+    </div>
+
+    <div class="row">
+         <div class="col-sm-6">
+           <?= $form->field($model, 'ket')->textInput(['maxlength' => true]) ?>
+
+        </div>
+    </div>
+
+
+             <div class="col-sm-12">
+        <?= Html::submitButton($model->isNewRecord ? 'Simpan' : 'Perbaharui', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+            </div>
     <?php ActiveForm::end(); ?>
 
+
+</div>
 </div>
