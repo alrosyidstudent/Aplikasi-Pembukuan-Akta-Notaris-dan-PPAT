57230835b2c96bfd11cfe454b88962476082419d
Andy Ferbiantoro <andyfebri742@gmail.com>
2020-04-14 20:52:05 +0700

    Mengubah index intuk menampilkan data Akun dan menu CRUD menggunakan generate Gii

--- views/akun/index.php
+++ views/akun/index.php
@@ -1,73 +1,41 @@
-<?php 
+<?php
 
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
-use app\models\User;
-use app\models\Akun;
-use yii\widgets\ActiveForm;
-use kartik\date\DatePicker;
-use kartik\widgets\Select2;
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
+$this->title = 'Kelola Akun';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="akun-index">
 
-
-$this->title='kelola akun';
-$this->params['breadcrubs'][] = $this->title;
- ?>
- <div class="akun">
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php  ?>
- </div>
-    <p>
-     
 
-       <?= Html::a('Tambah akun', ['create'], ['class' => 'btn btn-success']) ?>
-    
+    <p>
+        <?= Html::a('Tambah Akun', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-    
-     
 
 <div class="panel panel-primary">
-    <div class="panel-heading">List Pemasukan</div>   
-      <div class="x_panel">
-    
-        <table class="table table-bordered table-hover">
-            <tr class="success" align="center">
-                <td>nama</td>
-                <td>debit</td>
-                <td>kredit</td>
-                <td>Keterangan</td>
-                <td>Aksi</td>
+    <div class="panel-heading">List Akun</div>   
+        <div class="x_panel">
+<?php Pjax::begin(); ?>    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
 
-                
+            'nama',
+            'debit',
+            'kredit',
 
-            </tr>
-            <?php 
-            foreach ($dataAkun as $akun) {
-                # code...
-                ?>
-                <tr>
-                    <td><?=$akun->nama ?></td>
-                     <td><?=$akun->debit ?></td>
-                    <td>Rp.<?=$akun->kredit ?>,-</td>
-                    <td><?=$akun->ket ?></td>
-                    <td align="center"> 
-                        <?= Html::a('Hapus', ['delete', 'id' => $akun->id], [
-                         'class' => 'btn btn-danger',
-                            'data' => [
-                                'confirm' => 'Apakah anda yakin akan menghapus akun ini?',
-                                'method' => 'post',
-                                    ],
-                                ]) ?>
-                             <?= Html::a('Perbaharui', ['update', 'id' => $akun->id], ['class' => 'btn btn-primary']) ?>
-                    </td>
-                </tr>
-            <?php 
-            }
-            ?>
+            // 'kategori_akun_id',
+            // 'ket',
 
-        </table>
-
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+<?php Pjax::end(); ?>
+        </div>
     </div>
 </div>
-
