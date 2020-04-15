d6b000d10fede59ed75b6f5049c1e028c510693c
muhamadfatwa <fatwa.alrosyid.ti10.poliwangi@gmail.com>
2020-04-15 12:20:25 +0700

    Menampilkan data akta pada bulan tertentu

--- views/laporan-pencatatan-akta/index.php
+++ views/laporan-pencatatan-akta/index.php
@@ -1,53 +1,90 @@
 <?php
 
 use yii\helpers\Html;
-use kartik\grid\GridView;
+use yii\grid\GridView;
 use yii\widgets\Pjax;
-use app\models\User;
-
 /* @var $this yii\web\View */
-/* @var $searchModel app\models\AktaBadanSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Laporan Pencatatan Akta';
+$this->title = 'Akta Notaris';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="laporan-pencatatan-akta-index">
+<div class="akta-notaris-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="panel panel-primary">
-        <div class="panel-heading" style="height: 40px;">Daftar Laporan Pencatatan Akta</div>
-            <table class="table table-striped table-hover table-condensed" style="margin-top: 20px;">
-                <thead>
-                            <tr style="height: 35px;">
-                                <td width="4%" align="center" valign="middle"><b>#</b></td>
-                                <td width="25%" class="align-middle"><b>Periode Bulan</b></td>
-                                <td width="64%" class="align-middle"><b>Keterangan</b></td>
-                                <td >Detail</td>
-                            </tr>    
-                </thead>
-                <tbody>
-                    <?php
-                    $no = 1;
-                      foreach ($Bulan as $bln) {
-                         
-                    ?>
-                            <tr>
-                                <td width="4%" align="center"><?php echo $no; ?></td>
-                                <td width="25%" class="align-middle"><?php echo $bln; ?></td>
-                                <td width="64%" class="align-middle">Laporan Pencatatan Akta Periode Bulan <?php echo $bln; ?></td>
-                                <td ><?= Html::a('Lihat', ['detail'], ['class' => 'btn btn-xs btn-success', 'bln' => $bln]) ?></td>
-                            </tr>
-                    <?php
-                      $no++;
-                  }
-                    ?>
-                </tbody>    
-            </table>
-        <div class="panel-footer"></div>  
-    </div>
-
 </div>
+<div class="panel panel-primary">
+        <div class="panel-heading"></div>
+        <div class="x_panel">
+            <table class="table table-striped table-hover table-condensed">
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Januari</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['januari'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Februari</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['februari'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Maret</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['maret'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan April</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['april'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Mei</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['mei'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Juni</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['juni'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Juli</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['juli'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Agustus</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['agustus'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Septembert</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['septembert'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Oktober</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['oktober'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan November</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['november'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+                <tr>
+                    <td >Laporan Pencatatan Akta Periode Bulan Desember</td>
+                    <td width="10%"><?= Html::a('Lihat Laporan', ['desember'], ['class' => 'btn btn-sm btn-success']) ?></td>
+                </tr>
+            </table>
 
 
+
+<!-- <?php Pjax::begin(); ?>    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'notaris_id',
+            'nomor',
+            'tanggal',
+            'nama',
+            'akta_notaris_jenis_id',
+            'register',
+            'corporate_client_id',
+            'user_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+<?php Pjax::end(); ?> -->
