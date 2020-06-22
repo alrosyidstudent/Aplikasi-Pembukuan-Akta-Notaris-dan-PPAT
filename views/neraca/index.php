f672751e4a99a2c58cf83aab8de789ea8f9da54c
andyferbiantoro <54011276+andyferbiantoro@users.noreply.github.com>
2020-06-22 10:43:11 +0700

    laporan neraca

--- views/neraca/index.php
+++ views/neraca/index.php
@@ -63,7 +63,7 @@ $this->params['breadcrumbs'][] = $this->title;
         
     </div>
   <?= Html::submitButton('Filter', ['class' => 'btn btn-primary']) ?>  
-   <button class="btn btn-primary" onclick="print('PDFtoPrint')">Cetak pdf</button>      
+   <button class="btn btn-warning" onclick="print('PDFtoPrint')">Cetak pdf</button>      
   
     
  <?php ActiveForm::end(); ?>
@@ -95,7 +95,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <tr class="success" align="left" >
                 <td class="font-weight-bold">Aset Lancar</td>
                 <td></td>
-                <td></td>               
+                <td>Total</td>               
             </tr>
             <?php 
             $totalAsetLancar=0;
@@ -104,7 +104,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ?>
             <tr>                      
                 <td><?=$lancar->nama ?></td>
-                <td>Rp.<?=$lancar->debit ?>,-</td>
+                <td>Rp. <?=number_format($lancar->debit, 0, ".", ".")?>,00</td>
                 <td></td>
 
             </tr>
@@ -114,7 +114,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <tr class="warning">
                 <td>TOTAL ASET LANCAR</td>
                 <td> </td>
-                <td class="font-weight-bold">Rp.<?=$totalAsetLancar?>,- </td>
+                <td class="font-weight-bold">Rp. <?=number_format($totalAsetLancar, 0, ".", ".")?>,00</td>
             </tr>
 
 
@@ -131,7 +131,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     ?>
                     <tr>                      
                         <td><?=$tetap->nama ?></td>
-                        <td>Rp.<?=$tetap->debit ?>,-</td>
+                        <td>Rp. <?=number_format($tetap->debit, 0, ".", ".")?>,00</td>
                         <td></td>
 
                     </tr>
@@ -141,18 +141,48 @@ $this->params['breadcrumbs'][] = $this->title;
                     <tr class="warning">
                         <td class="font-weight-bold">TOTAL ASET TETAP</td>
                         <td> </td>
-                        <td class="font-weight-bold">Rp.<?=$totalAsetTetap?>,- </td>
+                        <td class="font-weight-bold">Rp. <?=number_format($totalAsetTetap, 0, ".", ".")?>,00 </td>
                     </tr>
+
+
+                    <!-- Depresiasi -->
+                    <tr class="success" align="left" >
+                        <td class="font-weight-bold">Depresiasi</td>
+                        <td></td>
+                        <td></td>               
+                    </tr>
+                    <?php 
+                    $TotalDepresiasi=0;
+                    foreach ($dataDepresiasi as $depresiasi) {
+                        $TotalDepresiasi+=$depresiasi['nominal'];
+                    ?>
+                    <tr>                      
+                        <td><?=$depresiasi->keterangan ?></td>
+                        <td>( Rp. <?=number_format($depresiasi->nominal, 0, ".", ".")?>,00 )</td>
+                        <td></td>
+
+                    </tr>
+                    <?php  
+                    }
+                    ?>
+                    <tr class="warning">
+                        <td class="font-weight-bold">TOTAL DEPRESIASI</td>
+                        <td> </td>
+                        <td class="font-weight-bold">( Rp. <?=number_format($TotalDepresiasi, 0, ".", ".")?>,00 )</td>
+                    </tr>
+
+
+
 
             <!-- TOTAL AKTIVA -->
             <?php 
             $totalAset=0;
-            $totalAset=$totalAsetLancar+$totalAsetTetap;
+            $totalAset=($totalAsetLancar+$totalAsetTetap)-$TotalDepresiasi;
             ?>
             <tr class="danger">
-                <td class="font-weight-bold">TOTAL ASET</td>
+                <td class="font-weight-bold" style="color: blue">TOTAL ASET</td>
                 <td></td>
-                <td class="font-weight-bold">Rp.<?=$totalAset?>,- </td>
+                <td class="font-weight-bold" style="color: blue">Rp. <?=number_format($totalAset, 0, ".", ".")?>,00 </td>
             </tr>
 
 
@@ -163,23 +193,43 @@ $this->params['breadcrumbs'][] = $this->title;
                 <td></td>               
             </tr>
             <?php 
-            $totalekuitas=0;
+            $totalmodal=0;
             foreach ($dataEkuitas as $modal) {
-                $totalekuitas+=$modal['kredit'];
+                $totalmodal+=$modal['kredit'];
             ?>
             <tr>                      
-                <td><?=$modal->nama ?></td>
-                <td>Rp.<?=$modal->kredit ?>,-</td>
+                <td><?=$modal->nama ?> (Kredit)</td>
+                <td>Rp. <?=number_format($modal->kredit, 0, ".", ".")?>,00</td>
                 <td></td>
 
             </tr>
+
             <?php  
             }
             ?>
+             <?php 
+            $totalmodal2=0;
+            foreach ($dataEkuitas as $modal) {
+                $totalmodal2+=$modal['debit'];
+            ?>
+            <tr>                      
+                <td><?=$modal->nama ?> (Debit)</td>
+                <td>( Rp. <?=number_format($modal->debit, 0, ".", ".")?>,00 )</td>
+                <td></td>
+
+            </tr>
+            
+            <?php  
+            }
+            ?>
+            <?php 
+            $totalmodal3=0;
+            $totalmodal3=$totalmodal-$totalmodal2;
+             ?>
             <tr class="warning">
                 <td class="font-weight-bold">TOTAL MODAL
                 <td> </td>
-                <td class="font-weight-bold">Rp.<?=$totalekuitas?>,- </td>
+                <td class="font-weight-bold">Rp. <?=number_format($totalmodal3, 0, ".", ".")?>,00 </td>
             </tr>
 
 
@@ -196,7 +246,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     ?>
                     <tr>                      
                         <td><?=$hutang->nama ?></td>
-                        <td>Rp.<?=$hutang->kredit ?>,-</td>
+                        <td> Rp. <?=number_format($hutang->kredit, 0, ".", ".")?>,00</td>
                         <td></td>
 
                     </tr>
@@ -206,27 +256,83 @@ $this->params['breadcrumbs'][] = $this->title;
                     <tr class="warning">
                         <td class="font-weight-bold">TOTAL LIABILITAS/HUTANG</td>
                         <td> </td>
-                        <td class="font-weight-bold">Rp.<?=$totalHutang?>,- </td>
+                        <td class="font-weight-bold"> Rp. <?=number_format($totalHutang, 0, ".", ".")?>,00 </td>
                     </tr>
 
 
                     <!-- TOTAL PASIVA -->
             <?php 
             $totalPasiva=0;
-            $totalPasiva=$totalekuitas+$totalHutang;
+            $totalPasiva=$totalmodal3+$totalHutang;
             ?>
             <tr class="danger">
-                <td class="font-weight-bold">TOTAL LIABILITAS DAN MODAL</td>
+                <td class="font-weight-bold" style="color: blue">TOTAL LIABILITAS DAN MODAL</td>
                 <td></td>
-                <td class="font-weight-bold">Rp.<?=$totalPasiva?>,- </td>
+                <td class="font-weight-bold" style="color: blue"> Rp. <?=number_format($totalPasiva, 0, ".", ".")?>,00 </td>
             </tr>
 
+       
 
         </table>
         
     </div>
+
+
+
+                <!-- Pesan jika tidak balance -->
+            <div>
+                
+                 <table class="table table-hover">
+                
+                 <?php if ($totalAset!=$totalPasiva) {
+                    # code...
+                 ?>    
+                 <?php 
+                 $selisih=0;
+                 $selisih=$totalAset-$totalPasiva;
+                 $selisih2=0;
+                 $selisih2=$totalPasiva-$totalAset;
+                ?>
+                <tr class="danger">
+                    <td style="color: red">     
+                          Periksa kembali data Akun anda, Total Aset dengan Total Liabailitas dan Modal tidak Balance   
+                    </td>       
+                </tr>
+                <tr class="danger">
+                    <td style="color: red">Total Aset adalah Rp. <?=number_format($totalAset, 0, ".", ".")?>,00</td>
+                </tr>
+                <tr class="danger">
+                    <td style="color: red">Total Liabilitas dan Modal adalah Rp. <?=number_format($totalPasiva, 0, ".", ".")?>,00</td>
+                </tr>
+                <tr class="danger">
+
+                    <td style="color: red">
+                    <?php if ($totalAset>$totalPasiva): ?>
+                        Buat Akun Ekuitas Modal dan tambahkan selisih sebesar Rp. <?=number_format($selisih, 0, ".", ".")?>,00 pada kredit?
+                         <br><br><?= Html::a('Tambahkan', ['akun/createkredit','nama'=>'Ekuitas Modal','kredit'=>$selisih], ['class' => 'btn btn-primary']) ?>
+                    <?php endif ?>
+
+                    <?php if ($totalAset<$totalPasiva): ?>
+                        Buat Akun Ekuitas Modal dan tambahkan selisih sebesar Rp. <?=number_format($selisih2, 0, ".", ".")?>,00 pada debit?
+                         <br><br><?= Html::a('Tambahkan', ['akun/createdebit','nama'=>'Ekuitas Modal','debit'=>$selisih2], ['class' => 'btn btn-primary']) ?>
+                    <?php endif ?>
+                       
+                    </td>
+                </tr>
+                
+                
+                <?php 
+                }else{
+                } ?>    
+                </table>
+
+                    
+            </div>
 </div>
 </div>
+
+
+<!-- fungsi untuk mencetak laporan ke dalam pdf -->
 <script type="text/javascript">
 function print(elem) {
     var mywindow = window.open('', 'PRINT', 'height=800,width=1000');
